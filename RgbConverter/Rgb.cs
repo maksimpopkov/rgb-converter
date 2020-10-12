f78c0f4803b8e5f650e1dbd20a668f937f6081a7
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-10-12 06:04:36 +0000

    Upload New File

---
+++ RgbConverter/Rgb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RgbConverter
+{
+    public static class Rgb
+    {
+        /// <summary>
+        /// Gets hexadecimal representation source RGB decimal values.
+        /// </summary>
+        /// <param name="red">The valid decimal value for RGB is in the range 0-255.</param>
+        /// <param name="green">The valid decimal value for RGB is in the range 0-255.</param>
+        /// <param name="blue">The valid decimal value for RGB is in the range 0-255.</param>
+        /// <returns>Returns hexadecimal representation source RGB decimal values.</returns>
+        public static string GetHexRepresentation(int red, int green, int blue)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
